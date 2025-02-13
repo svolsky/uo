282c4970c2e33ab18806bc3187dbdf7f23afef2f
stas volski <stas.volsky@gmail.com>
2025-02-13 09:44:35 +0100

    stealth ninja

---
+++ train_hide_stealth_ninja.py
@@ -0,0 +1,34 @@
+# MUST HAVE REQUIRED NINJA TO ATTEMPT SHADOWJUMP
+# best to start in an area between north south trees with visible tiles between
+
+Y_1 = 1274
+Y_2 = 1265
+
+
+def get_direction():
+    if Player.Position.Y <= Y_2:
+        return 'South'
+    elif Player.Position.Y >= Y_1:
+        return 'North'
+    else:
+        return Player.Direction
+    
+
+while not Player.IsGhost:   
+    if Player.Visible:
+        Target.Cancel()        
+        Player.UseSkill("Hiding")
+        Misc.Pause(3000)
+    
+    if not Player.Visible:
+        dir = get_direction()
+        Player.Walk(dir)
+        Player.Walk(dir)
+        
+    if not Player.Visible and Player.Mana > 15: 
+        Spells.CastNinjitsu("Shadowjump")
+        Target.WaitForTarget(3000, False)
+        Target.TargetExecuteRelative(Player.Serial,-1)
+        Misc.Pause(3000)
+  
+    Misc.Pause(400)
